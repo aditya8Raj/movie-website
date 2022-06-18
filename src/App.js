e4de6a6610cb625d3891465511dfcc767fc167b1
Aditya Raj <aditya8raj000@gmail.com>
2022-06-18 21:05:56 +0530

    it should work now

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import MovieCard from "./components/MovieCard";
 import SearchIcon from "./search.png";
 import "./App.css";
 
-const API_URL = "http://www.omdbapi.com?apikey=1ee10642";
+const API_URL = "https://www.omdbapi.com?apikey=1ee10642";
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
